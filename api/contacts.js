49b0118bd9a67dd088f5edf7eb0fd43a35a64ea6
Catherine Thompson <cathythompson0915@gmail.com>
2017-04-14 18:10:41 -0600

    send message api

--- api/contacts.js
+++ api/contacts.js
@@ -13,6 +13,28 @@ export function fetchContacts () {
     .then(status)
 }
 
+export function sendMessage (contact, message) {
+  const params = {
+    recipients: [
+      'contact-' + contact.id
+    ],
+    message: {
+      body: message
+    }
+  }
+
+  return fetch(BASE_URL + '/api/v1/messages' +
+    '?' + 'token=' + TOKEN,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    })
+    .then(status)
+}
+
 const status = async (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response.json()
--- screens/ContactDetailsScreen.js
+++ screens/ContactDetailsScreen.js
@@ -3,7 +3,10 @@ import {
   View,
   Text,
   StyleSheet,
+  Image,
+  TextInput
 } from 'react-native'
+import { sendMessage } from '../api/contacts.js'
 
 export default class ContactDetailsScreen extends Component {
   static route = {
@@ -13,9 +16,28 @@ export default class ContactDetailsScreen extends Component {
   }
 
   render () {
+    const { contact } = this.props
+    sendMessage(contact, 'from my app')
     return (
       <View style={styles.container}>
-        <Text>Contact</Text>
+        <Image
+          style={styles.iconImage}
+          source={{uri: contact.avatar_url}} />
+        <Text>{contact.first_name + ' ' + contact.last_name}</Text>
+        <Text>Mobile Number:  {contact.phone_mobile}</Text>
+        {
+          contact.email ? <Text>Email:  {contact.email}</Text> : null
+        }
+        {
+          contact.street_address ? (
+            <View>
+              <Text>Address: </Text>
+              <Text>{contact.street_address}</Text>
+              <Text>{contact.city}, {contact.state} {contact.zip_code}</Text>
+            </View>
+          ) : null
+        }
+        <TextInput />
       </View>
     )
   }
@@ -24,5 +46,14 @@ export default class ContactDetailsScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center'
+  },
+  iconImage: {
+    width: 70,
+    height: 70,
+    margin: 12,
+    resizeMode: 'contain',
+    borderWidth: 1,
+    borderRadius: 35
   }
 })
