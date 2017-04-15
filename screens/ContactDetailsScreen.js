571c22887d1c77ab8efcafad93c3e2ed09a0a185
Catherine Thompson <cathythompson0915@gmail.com>
2017-04-14 19:05:13 -0600

    text input view

--- screens/ContactDetailsScreen.js
+++ screens/ContactDetailsScreen.js
@@ -4,9 +4,11 @@ import {
   Text,
   StyleSheet,
   Image,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native'
 import { sendMessage } from '../api/contacts.js'
+import { FontAwesome } from '@expo/vector-icons'
 
 export default class ContactDetailsScreen extends Component {
   static route = {
@@ -15,45 +17,93 @@ export default class ContactDetailsScreen extends Component {
     }
   }
 
+  state = {
+    message: ''
+  }
+
   render () {
     const { contact } = this.props
-    sendMessage(contact, 'from my app')
+    // sendMessage(contact, 'from my app')
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.iconImage}
-          source={{uri: contact.avatar_url}} />
-        <Text>{contact.first_name + ' ' + contact.last_name}</Text>
-        <Text>Mobile Number:  {contact.phone_mobile}</Text>
-        {
-          contact.email ? <Text>Email:  {contact.email}</Text> : null
-        }
-        {
-          contact.street_address ? (
-            <View>
-              <Text>Address: </Text>
-              <Text>{contact.street_address}</Text>
-              <Text>{contact.city}, {contact.state} {contact.zip_code}</Text>
-            </View>
-          ) : null
-        }
-        <TextInput />
+
+        <View>
+          <Image
+            style={styles.avatarImage}
+            source={{uri: contact.avatar_url}} />
+          <Text>{contact.first_name + ' ' + contact.last_name}</Text>
+          <Text>Mobile Number:  {contact.phone_mobile}</Text>
+          {
+            contact.email ? <Text>Email:  {contact.email}</Text> : null
+          }
+          {
+            contact.street_address ? (
+              <View>
+                <Text>Address: </Text>
+                <Text>{contact.street_address}</Text>
+                <Text>{contact.city}, {contact.state} {contact.zip_code}</Text>
+              </View>
+            ) : null
+          }
+        </View>
+
+        <View style={styles.messageContainer}>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholder='send message'
+              onChangeText={this._handleTextChange}
+              style={styles.textInput} />
+          </View>
+          <TouchableOpacity
+            style={styles.sendButtonContainer}
+            onPress={() => {}}>
+            <FontAwesome
+              name='send'
+              size={24}
+              style={{}}/>
+          </TouchableOpacity>
+        </View>
+
       </View>
     )
+  }
+
+  _handleTextChange = (message) => {
+    this.setState({ message: message })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    justifyContent: 'space-between'
   },
-  iconImage: {
+  avatarImage: {
     width: 70,
     height: 70,
     margin: 12,
     resizeMode: 'contain',
     borderWidth: 1,
     borderRadius: 35
+  },
+  messageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 16,
+  },
+  textInputContainer: {
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 16,
+    flex: 1
+  },
+  textInput: {
+    fontWeight: '300',
+    backgroundColor: 'transparent',
+    fontSize: 18,
+    height: 30,
+  },
+  sendButtonContainer: {
+    paddingLeft: 16
   }
 })
